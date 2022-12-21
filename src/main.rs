1c8aee402c0d6796c4453fd02320290364f617e8
BonMalYan <jan.gra.ser.001@gmail.com>
2022-12-21 17:33:33 +0100

    Base mechanics implemented

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,47 @@
 use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
-
 fn main() {
-	println!("Guess the number!");
-	let secret_number = rand::thread_rng().gen_range(1..=100);
-	//println!("The secret number is: {secret_number}");
-
-	loop {
-		println!("Please input your guess.");
-
-		let mut guess = String::new();
-
-		io::stdin()
-			.read_line(&mut guess)
-			.expect("Failed to read line");
-
-		println!("You guessed: {guess}");
-		
-		let guess: u32 = match guess.trim().parse() {
-			Ok(num) => num,
-			Err(_) => { println!("Please type a number!"); continue},
-		};
-
-		match guess.cmp(&secret_number) {
-			Ordering::Less => println!("Too small!"),
-			Ordering::Greater => println!("Too big!"),
-			Ordering::Equal =>{ 
-				println!("You win!");
-				break;
-			},
-	}
+	starting_msgs();
+	let start = get_number("Please enter the start of the sequence");
+	let max = get_number("Please enter the number of iterations");
+	fibb(start, max, 0, 1);
 }
+
+fn starting_msgs(){
+	println!("Hello there 👋");
+	println!("This program prints the fibonacci sequence with a given start and number of iterations.");
+	println!("After some iterations the fibonacci sequence goes very high!");
+	println!("This program reaches up to iteration 185. Which is 205697230343233228174223751303346572685");
+	print!("To start ");
+}
+
+fn get_number(msg: &str)-> u128{
+	let mut _num = 0;
+	loop{
+		let mut input = String::new();
+		println!("{msg}");
+		io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+		_num = match input.trim().parse() {
+			Ok(num) => num,
+			Err(_) => {println!("Error thats not a number! Consider using just positive numbers"); continue},
+		};
+		break;
+	}
+	_num
+}
+
+fn fibb(start: u128, mut max: u128, num: u128, next: u128){
+	if max > 0 && num <= (u128::MAX - next)
+	{
+		//print once we start
+		if start <= num {
+			println!("{num} ");
+			//print!("{max} ");
+			max = max - 1;
+		}
+		//recursive call
+		fibb(start, max, next, num+next);
+	}
 }
